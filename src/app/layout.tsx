12af5f6f90736cfcf8421770b7a2c0d331ea9fd9
unknown <abiyyurahid20@gmail.com>
2026-05-17 21:54:49 +0700

    feat: using maze tracker script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { AuthProvider } from '@/context/AuthContext'
 import { AppProvider } from '@/lib/store'
+import Script from 'next/script'
 
 const ibmPlexSans = IBM_Plex_Sans({
 	variable: '--font-ibmPlex-sans',
@@ -43,6 +44,37 @@ export default function RootLayout({
 						<Toaster position='bottom-right' richColors closeButton />
 					</AuthProvider>
 				</QueryProvider>
+
+				<Script id="maze-universal-snippet" strategy="afterInteractive">
+					{`
+						(function (m, a, z, e) {
+							var s, t, u, v;
+							try {
+								t = m.sessionStorage.getItem('maze-us');
+							} catch (err) {}
+
+							if (!t) {
+								t = new Date().getTime();
+								try {
+									m.sessionStorage.setItem('maze-us', t);
+								} catch (err) {}
+							}
+
+							u = document.currentScript || (function () {
+								var w = document.getElementsByTagName('script');
+								return w[w.length - 1];
+							})();
+							v = u && u.nonce;
+
+							s = a.createElement('script');
+							s.src = z + '?apiKey=' + e;
+							s.async = true;
+							if (v) s.setAttribute('nonce', v);
+							a.getElementsByTagName('head')[0].appendChild(s);
+							m.mazeUniversalSnippetApiKey = e;
+						})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '3aff206f-c4ab-488e-ba08-0584c1b5eacd');
+					`}
+				</Script>
 			</body>
 		</html>
 	)
